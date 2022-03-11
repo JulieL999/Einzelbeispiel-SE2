6c9ee5426106f3a1989bfe53539bc8ccc3adcb16
Julia Lomonosova <julie.lomonosova283@gmail.com>
2022-03-11 18:44:47 +0100

    Aufgabe 1

--- app/src/main/java/com/example/networktest/ClientThread.java
+++ app/src/main/java/com/example/networktest/ClientThread.java
@@ -1,2 +1,43 @@
-package com.example.networktest;public class ClientThread {
+package com.example.networktest;
+
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientThread implements Runnable {
+    private static final int SERVER_PORT = 53212;
+    private static final String SERVER_DOMAIN = "se2-isys.aau.at";
+
+    private String matrNum;
+    private String modifiedMatrNum;
+
+    public ClientThread() {}
+
+    public ClientThread(String matrNum) {
+        this.matrNum = matrNum;
+    }
+
+    public void run() {
+        try {
+            Socket socket = new Socket("143.205.174.165", SERVER_PORT);
+            Log.i("info", "run method works");
+            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outToServer.writeBytes(matrNum + '\n');
+            modifiedMatrNum = inFromServer.readLine();
+            Log.i("info","FROM Server: " + modifiedMatrNum);
+            socket.close();
+        }
+        catch (Exception e) {
+            System.err.println(e.getStackTrace());
+        }
+    }
+
+    public String getMatrNum() {
+        return modifiedMatrNum;
+    }
+
 }
--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSend = findViewById(R.id.button);
+        btnSend.setOnClickListener(MainActivity.this);
     }
+
+    public void onClick(View view)  {
+        try {
+            EditText et = findViewById(R.id.matrNum);
+            String matrNum = et.getText().toString();
+            ClientThread t = new ClientThread(matrNum);
+            Thread newThread = new Thread(t);
+            newThread.start();
+            newThread.join();
+            TextView antwort = findViewById(R.id.antwortServer);
+            antwort.setText(t.getMatrNum());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
